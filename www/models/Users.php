142b5094543f5df1dd7f8342e947061540798146
Delcross <43371445+Delcross@users.noreply.github.com>
2019-05-11 16:16:48 +0200

    Add files via upload

--- www/models/Users.php
+++ www/models/Users.php
@@ -110,11 +110,12 @@ class Usersget extends BaseSQL
                         'submit' => 'Se connecter',
                         'reset' => 'Annuler', ],
 
-                    'data' => [
-                            'email' => ['type' => 'email', 'placeholder' => 'Votre email', 'required' => true, 'class' => 'form-control', 'id' => 'email',
-                                'error' => "L'email n'est pas valide", ],
+                        'data' => ['email' => ['type' => 'email', 'placeholder' => 'Votre email', 
+                        'required' => true, 'class' => 'form-control', 'id' => 'email',
+                            'error' => "L'email n'est pas valide", ],
 
-                            'pwd' => ['type' => 'password', 'placeholder' => 'Votre mot de passe', 'required' => true, 'class' => 'form-control', 'id' => 'pwd',
+                        'pwd' => ['type' => 'password', 'placeholder' => 'Votre mot de passe', 
+                        'required' => true, 'class' => 'form-control', 'id' => 'pwd',
                                 'error' => 'Veuillez préciser un mot de passe', ],
                     ],
                 ];
